32d44adc8bd0d62eb7aec1dc809d050767958b42
BillStark001 <BillStark001@users.noreply.github.com>
2018-01-09 15:24:25 +0800

    video stream
    video stream

---
+++ vstream/vstream.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+cap = cv2.VideoCapture(0)
+
+if __name__=='main':
+    while(1):
+        ret, frame = cap.read()
+        cv2.imshow("capture", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):break
+        cap.release()
+        cv2.destroyAllWindows() 
